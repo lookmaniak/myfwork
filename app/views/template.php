ca0e93f4d28e23f07e288e3e00c2ea3cfd406a4b
Lukman Nulhakim <lookman.nh@gmail.com>
2024-12-16 16:14:26 +0700

    fix sample template

--- app/views/template.php
+++ app/views/template.php
@@ -17,18 +17,20 @@ class Layout extends View {
                 tag('nav.navbar.navbar-expand.navbar-light.bg-light', [
                     tag('div.container-fluid', [
                         tag('a.navbar-brand[href="#"]', 'MyFWork'),
+                        tag('ul.nav.d-flex.d-md-none.d-block', [
+                            tag('li.nav-item', tag('a.nav-link[href="'.path('posts').'"]', 'Articles')),
+                            tag('li.nav-item', tag('a.nav-link[href="'.path('about').'"]', 'About')),
+                        ]),
                         tag('button.navbar-toggler[type="button"][data-bs-toggle="collapse"][data-bs-target="#navbarNav"][aria-controls="navbarNav"][aria-expanded="false"][aria-label="Toggle navigation"]', tag('span.navbar-toggler-icon')),
                     ])
                 ]),
                 tag('div.container-fluid', [
                     tag('div.row', [
-                        tag('div#sidebar.col-md-3.col-lg-2.d-md-block.bg-light.sidebar', [
+                        tag('div#sidebar.col-md-3.col-lg-2.d-md-block.bg-light.sidebar.d-none.d-md-block', [
                             tag('ul.nav.flex-column', [
                                 tag('li.nav-item', tag('a.nav-link[href="'.path('posts').'"]', 'Articles')),
-                            ]),
-                            tag('ul.nav.flex-column', [
                                 tag('li.nav-item', tag('a.nav-link[href="'.path('about').'"]', 'About')),
-                            ])
+                            ]),
                         ]),
                         tag('div#main.col-md-9.col-lg-10', $content)
                     ])
